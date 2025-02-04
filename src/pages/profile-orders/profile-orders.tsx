53f145407d3e30ecda7d26371d183d4cf28133a3
Artem Rarog <rarokss228@gmail.com>
2025-02-04 22:27:40 +0700

    1.2

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,9 +1,27 @@
 import { ProfileOrdersUI } from '@ui-pages';
-import { FC } from 'react';
-import { useSelector } from '../../services/store';
+import { FC, useEffect } from 'react';
+import { useSelector, useDispatch } from '../../services/store';
+import { getUserOrders } from '../../services/slices/userSlice';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const { orders } = useSelector((state) => state.userSlice);
+  const { orders, isAuthChecked, loading, error } = useSelector(
+    (state) => state.userSlice
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isAuthChecked && orders.length === 0) {
+      dispatch(getUserOrders());
+    }
+  }, [isAuthChecked, orders, dispatch]);
+
+  if (loading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (error) {
+    return <div>Ошибка загрузки заказов. Попробуйте позже.</div>;
+  }
+
   return <ProfileOrdersUI orders={orders} />;
 };
